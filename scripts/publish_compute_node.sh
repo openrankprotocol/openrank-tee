b57f30e6105426bb68646cc6e8961b4a4f7edda4
lazovicff <github.grumbling276@passinbox.com>
2025-09-04 18:06:51 +0200

    Publish new compute node

---
+++ scripts/publish_compute_node.sh
@@ -0,0 +1,6 @@
+docker build . -f app/Dockerfile -t lazovicff/openrank-computer
+docker push lazovicff/openrank-computer
+
+devkit avs build --context=testnet
+
+devkit avs release publish --context=testnet --registry=docker.io/lazovicff/openrank-computer
